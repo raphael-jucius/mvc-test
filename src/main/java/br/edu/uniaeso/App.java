4e6837ae4e07ed3358d83b26684cba5e45be1eb6
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-05-09 20:17:57 -0300

    call to jframe

--- src/main/java/br/edu/uniaeso/App.java
+++ src/main/java/br/edu/uniaeso/App.java
@@ -4,10 +4,8 @@ package br.edu.uniaeso;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        TelaVotacao tv = new TelaVotacao();
     }
 }
